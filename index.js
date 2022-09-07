ec62236a8f6c047bf28c985350fa8b52207652e4
palminski <williambolls98@gmail.com>
2022-09-07 14:48:26 -0700

    added ability to input information to the application

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "projectTitle",
+        message: "what is your project's title?"
+    },
+    {
+        type: "input",
+        name: "author",
+        message: "Please enter who this project is being attributed to."
+    },
+    {
+        type: "input",
+        name: "testInputField",
+        message: "this is a test. please enter anything!"
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    console.log(questions);
+    inquirer
+        .prompt(questions)
+        .then(answers => console.log(answers));
+}
 
 // Function call to initialize app
 init();
