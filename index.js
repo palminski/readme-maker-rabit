c52737192f8b41963508ee2c0c498e4ea08cc438
palminski <williambolls98@gmail.com>
2022-09-07 14:58:51 -0700

    added all questions to be asked of users

--- index.js
+++ index.js
@@ -5,19 +5,49 @@ const fs = require("fs");
 const questions = [
     {
         type: "input",
-        name: "projectTitle",
+        name: "title",
         message: "what is your project's title?"
     },
     {
         type: "input",
-        name: "author",
-        message: "Please enter who this project is being attributed to."
+        name: "description",
+        message: "Please enter a description for this project."
     },
     {
         type: "input",
-        name: "testInputField",
-        message: "this is a test. please enter anything!"
-    }
+        name: "installation",
+        message: "Please enter installation instructions."
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please enter usage information for this project."
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Please enter contribution guidlines for this project."
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please enter test instructions for this project."
+    },
+    {
+        type: "input",
+        name: "licence",
+        message: "PLACEHOLDER FOR LICENCE INFO."
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "Please enter your github username."
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email adress."
+    },
 ];
 
 // TODO: Create a function to write README file
@@ -25,7 +55,7 @@ function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-    console.log(questions);
+    
     inquirer
         .prompt(questions)
         .then(answers => console.log(answers));
